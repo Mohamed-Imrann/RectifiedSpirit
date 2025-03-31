2e71433bbda3a5ca35b3427a630046c52f153e69
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-03-31 18:44:38 +0530

    Update gfilters.py

--- plugins/gfilters.py
+++ plugins/gfilters.py
@@ -8,7 +8,6 @@ from database.gfilters_mdb import(
    count_gfilters
 )
 
-from database.connections_mdb import active_connection
 from utils import get_file_id, gfilterparser, split_quotes
 from info import ADMINS
 
