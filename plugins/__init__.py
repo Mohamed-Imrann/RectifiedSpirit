c0e41b2f5fd693e21186d92f239d03bbf13cc2ce
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-07-19 11:15:37 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,23 @@
 from aiohttp import web
 from .route import routes
-from . import *
+
+# Explicitly import all plugin modules
+from . import auto
+from . import banned
+from . import broadcast
+from . import commands
+from . import crazy # Using the more advanced 'crazy.py' for series management
+from . import eval
+from . import fsub
+from . import genlink
+from . import get_file_id
+from . import gfilters
+from . import inline
+from . import join_req
+from . import p_ttishow
+from . import ping
+from . import pmfilter
+from . import request_forcesub
 
 
 async def web_server():
