32001844a1165a20885f8fcbbcfe6c723ed54e62
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-08-05 21:41:47 +0530

    Add files via upload

--- database/crazy_db.py
+++ database/crazy_db.py
@@ -8,7 +8,7 @@ logger.setLevel(logging.INFO)
 
 client = MongoClient(DATABASE_URI)
 db = client[DATABASE_NAME]
-series_collection = db.tseries
+series_collection = db.series
 
 def add_series(series_data):
     """Adds a new series document. Returns True on success, False on duplicate key error."""
