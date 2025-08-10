1c3b518c09ac82099f4be66e645afc92198138af
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-08-10 12:15:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -81,7 +81,6 @@ async def start_command(client, message):
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.HTML
         )
-        logger.info(message.command]
         return False
 
     if deep_link:
