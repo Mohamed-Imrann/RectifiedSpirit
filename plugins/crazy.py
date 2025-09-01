b235cdbad80ca36ae73d03439d24fa7c49690997
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-09-01 10:33:07 +0530

    Update crazy.py

--- plugins/crazy.py
+++ plugins/crazy.py
@@ -24,7 +24,7 @@ from database.crazy_db import (
     get_languages, delete_language, add_or_update_season, get_seasons, delete_season,
     add_or_update_quality, get_qualities, get_quality_link, delete_quality,
     get_poster_file_id, update_poster_file_id, publish_series, episodes_collection,
-    get_series, get_poster_manuel
+    get_series, get_poster_manuel, series_collection
 )
 from utils import (
     get_message_id, get_messages, delete_messages_from_user_chat, 
