e59587b0bd0fac3f77688ecc352b41d827a85b2b
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-10-01 20:59:17 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,17 @@
 from aiohttp import web
 from .route import routes
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Plugins package initialization
 
+All files in this directory will be automatically loaded as plugins
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info("📦 Plugins package loaded")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
