b3d66d111f0ed3baba26d322a20dc3138070a196
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-10-02 11:45:25 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,3 +1,4 @@
+from bot import Bot
 from pyrogram import Client, filters
 from utils import temp
 from pyrogram.types import Message
@@ -16,12 +17,12 @@ async def disabled_chat(_, client, message: Message):
 
 disabled_group=filters.create(disabled_chat)
 
-@Client.on_message(filters.private & banned_user & filters.incoming)
+@Bot.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
     await message.reply(f'<b>‼️Sorry Dude, You are Banned to use Me.‼️</b> \n\n<u>Ban Reason:</u> {ban["ban_reason"]}')
 
-@Client.on_message(filters.group & disabled_group & filters.incoming)
+@Bot.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
         InlineKeyboardButton('🧬 𝖲𝗎𝗉𝗉𝗈𝗋𝗍', url=f'https://t.me/https://t.me/raixchat')
