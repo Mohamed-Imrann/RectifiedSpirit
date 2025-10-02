e46323e45574c126c727c31e8f34546a71356a62
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-10-02 12:02:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# run_bot.py
-import asyncio
-from bot import Bot  # Import the Bot class from bot.py
+from bot import Bot
 
-async def main():
-    app = Bot()  # Initialize the bot
-    await app.start()  # Start the bot
-    await app.idle()  # Keep the bot running
-    await app.stop()  # Stop the bot when done
-
-if __name__ == "__main__":
-    asyncio.run(main())
+Bot().run()
