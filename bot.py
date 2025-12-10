dccc0d8a066275b9d74eec89a0208a4599011040
Mohamed Imran <77523586+Mohamed-Imrann@users.noreply.github.com>
2025-12-11 00:03:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,14 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN, 
-            workers=1000,
+            workers=300,
             plugins={"root": "plugins"},
-            sleep_threshold=2,
+            sleep_threshold=10,
+            request_timeout=60,     # MAIN FIX
+            connect_timeout=30,
+            read_timeout=30,
+            write_timeout=30,
+            ipv6=False              # VPS FIX
         )
 
     async def start(self, **kwargs):
@@ -65,29 +70,7 @@ class Bot(Client):
         limit: int,
         offset: int = 0,
     ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
+        
         current = offset
         while True:
             new_diff = min(200, limit - current)
