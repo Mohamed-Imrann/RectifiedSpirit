5a37c18e68906c0d9bf2d698e930e47f5205071e
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-12-26 08:14:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,11 +36,11 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7144888498').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://SpidySeries:SpidySeries@cluster0.xreosjj.mongodb.net/?retryWrites=true&w=majority")
+NEW_DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://SpidySeries:SpidySeries@cluster0.xreosjj.mongodb.net/?retryWrites=true&w=majority")
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # For migration
-NEW_DATABASE_URI = "mongodb+srv://sp:sp@cluster0.eh3zdcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://sp:sp@cluster0.eh3zdcl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MIGRATION_MODE = True  # Set to True during migration, then False after
 
 STIC = ('CAACAgUAAxkBAAJ5iGaibnAzoinskJ5B2LDpA66GpMKlAAIgAwAClbKhVx_qcsdH29wKHgQ CAACAgUAAxkBAAJ5hGaibjRA5dyi2XAQrO1JSxz4dXUuAALdBAACQ7YIVvz4vy3OyyQAAR4E CAACAgUAAxkBAAJ5jGaibp7C2RDX1OgS1ex0j3GedHoeAAJAAQACqjqpV0SUvBwyov-QHgQ').split()
