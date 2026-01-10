f5e209ad7e011b71c18112f4fe95bd2a00d1f47a
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2026-01-10 17:18:32 +0530

    Refactor banned user handling and chat restrictions

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,42 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from bot import Bot
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters
-from utils import temp
-from pyrogram.types import Message
 from database.users_chats_db import db
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from utils import temp
+
 
 async def banned_users(_, client, message: Message):
-    return (
-        message.from_user is not None or not message.sender_chat
-    ) and message.from_user.id in temp.BANNED_USERS
-
-banned_user = filters.create(banned_users)
+    return (message.from_user is not None or not message.sender_chat) and (message.from_user.id in temp.BANNED_USERS)
 
 async def disabled_chat(_, client, message: Message):
     return message.chat.id in temp.BANNED_CHATS
 
-disabled_group=filters.create(disabled_chat)
-
-@Bot.on_message(filters.private & banned_user & filters.incoming)
+@Client.on_message(filters.private & filters.incoming & filters.create(banned_users))
 async def ban_reply(bot, message):
-    ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'<b>‼️Sorry Dude, You are Banned to use Me.‼️</b> \n\n<u>Ban Reason:</u> {ban["ban_reason"]}')
+    ban = await db.get_cloud_ban_status(message.from_user.id)
+    await message.reply(f"‼️ <b>𝖲𝗈𝗋𝗋𝗒, 𝗒𝗈𝗎 𝖺𝗋𝖾 𝖻𝖺𝗇𝗇𝖾𝖽 𝖿𝗋𝗈𝗆 𝗎𝗌𝗂𝗇𝗀 𝗆𝖾.</b> ‼️\n\n<u>𝖡𝖺𝗇 𝖱𝖾𝖺𝗌𝗈𝗇:</u> {ban['ban_reason']}")
 
-@Bot.on_message(filters.group & disabled_group & filters.incoming)
+"""@Client.on_message(filters.group & filters.incoming & filters.create(disabled_chat))
 async def grp_bd(bot, message):
-    buttons = [[
-        InlineKeyboardButton('🧬 𝖲𝗎𝗉𝗉𝗈𝗋𝗍', url=f'https://t.me/https://t.me/raixchat')
-    ]]
-    reply_markup=InlineKeyboardMarkup(buttons)
-    vazha = await db.get_chat(message.chat.id)
-    k = await message.reply(
-        text=f"**𝖳𝗁𝗂𝗌 𝖢𝗁𝖺𝗍 𝖨𝗌 𝖭𝗈𝗍 𝖠𝗅𝗅𝗈𝗐𝖾𝖽.!\n𝖢𝗈𝗇𝗍𝖺𝖼𝗍 𝖬𝗒 𝖬𝖺𝗌𝗍𝖾𝗋.**\n<u>Reason:</u> <code>{vazha['reason']}</code>.",
-        reply_markup=reply_markup)
-    try:
-        await k.pin()
-    except:
-        pass
-    await bot.leave_chat(message.chat.id)
+    buttons = [[InlineKeyboardButton('👥 𝖠𝖽𝗆𝗂𝗇', url=BOT_ADMIN)]]
+    chat = await db.get_cloud_chat(message.chat.id)
+    k = await message.reply(text=f"<b>𝖠𝖼𝖼𝖾𝗌𝗌 𝗍𝗈 𝖳𝗁𝗂𝗌 𝖢𝗁𝖺𝗍 𝖨𝗌 𝖱𝖾𝗌𝗍𝗋𝗂𝖼𝗍𝖾𝖽!\n\n𝖯𝗅𝖾𝖺𝗌𝖾 𝖼𝗈𝗇𝗍𝖺𝖼𝗍 𝗍𝗁𝖾 𝖺𝖽𝗆𝗂𝗇 𝖿𝗈𝗋 𝖿𝗎𝗋𝗍𝗁𝖾𝗋 𝖽𝖾𝗍𝖺𝗂𝗅𝗌.</b>\n𝖱𝖾𝖺𝗌𝗈𝗇: <code>{chat['reason']}</code>.", reply_markup=InlineKeyboardMarkup(buttons))
+    try: await k.pin()
+    except: pass
+    await bot.leave_chat(message.chat.id)"""
