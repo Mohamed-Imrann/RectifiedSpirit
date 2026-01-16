628b4bf6694c52b1fc13be647b15a825fbd295e8
Mohamed Imran <77523586+Mohamed-Imrann@users.noreply.github.com>
2026-01-16 21:17:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,20 +1,21 @@
 # info.py
-API_ID = 123456
-API_HASH = "your_api_hash"
+API_ID = 31445120
 
-BOT_TOKEN = "your_bot_token"
+API_HASH = "5e6c06ca38688ec6ec6e65dbff6d9b07"
+
+BOT_TOKEN = "8554616651:AAHwOIWuR4o17xA0vO7yLBJ_J67a2RCT084"
 
 # ✅ User account session string (Pyrogram StringSession) - not bot token
-USER_SESSION = "your_user_session_string"
+USER_SESSION = "1BVtsOGYBu3IcLiO9NPvrRYP1aaYnkdB6duJbNGgeiBOuzrc9z0t1q2t2MrEGIW4a-MjpmkRpaLHKZkZyVcOjTqFVKwx0QLn9-bCpERJIWFDU6yGEP4NNOlSSYyL2dzGmjffaUabeNxx7zPEmjYLMcosg28HBDLgoi0Gpth0IpUfVzWzYup8NHuqM1WK9OS8iTw-s77f5EZ7D2FFf0pAfiFPhyeDByls-7sQ4XM09dIQhBHmAVuzvNqJliJ22RgvjudeYINfzJDJMOZwDDmXmuipB1STH-I1k5pIuKLB86BmrjTEVrbjrtgYs4ctaP_Rba9li_jSuWM-ju_7q-I79k-CLrHvibuU="
 
 # Admin user ids
-ADMINS = [3144888498]
+ADMINS = [7144888498]
 
 # Web port
 PORT = 8080
 
 # Your upload/source channel (must be YOUR channel)
-SOURCE_CHANNEL_ID = -1003319100929
+SOURCE_CHANNEL_ID = -1002400599577
 
 # Save delay per file (seconds)
 SAVE_DELAY = 1.2
