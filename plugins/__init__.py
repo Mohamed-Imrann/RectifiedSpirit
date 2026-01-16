28ac25cf6077e9c0e7188767c29cd44d8cb1a355
Mohamed Imran <77523586+Mohamed-Imrann@users.noreply.github.com>
2026-01-16 12:22:22 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,7 @@
 from aiohttp import web
-from .route import routes
-
+from web.routes import routes
 
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
+    app = web.Application(client_max_size=30_000_000)
+    app.add_routes(routes)
+    return app
