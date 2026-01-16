ba30ea28df36c01978d9ea640180e26dd68d317f
Mohamed Imran <77523586+Mohamed-Imrann@users.noreply.github.com>
2026-01-16 20:56:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,33 +1,22 @@
-import asyncio
+# utils.py
 from pyrogram.types import Message
-from info import AUTO_DELETE_SECONDS
 
-async def auto_delete(msg, sec: int = AUTO_DELETE_SECONDS):
-    if not sec or sec <= 0:
-        return
-    await asyncio.sleep(sec)
-    try:
-        await msg.delete()
-    except:
-        pass
 
-def get_file_id(msg: Message):
+def get_file_id(message: Message):
     """
-    Returns media object with .file_id and sets .message_type
+    Returns the media object containing file_id + message_type.
+    Supports: document, video, audio, photo
     """
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            "sticker",
-        ):
-            obj = getattr(msg, message_type, None)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
+    if message.document:
+        message.document.message_type = "document"
+        return message.document
+    if message.video:
+        message.video.message_type = "video"
+        return message.video
+    if message.audio:
+        message.audio.message_type = "audio"
+        return message.audio
+    if message.photo:
+        message.photo.message_type = "photo"
+        return message.photo
     return None
