14f30ba2b5f24d06d385dd16167d3662bb82d3d3
Mohamed Imran <77523586+Mohamed-Imrann@users.noreply.github.com>
2026-02-21 14:24:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,13 +14,13 @@ def is_enabled(value, default):
     else: return default
 
 # Required API Credentials and Bot Settings
-API_ID = '20400973' #bots
-API_HASH = '047838cb76d54bc445e155a7cab44664'
-BOT_TOKEN = '7448922857:AAEiLcjbuhP_XNSl0KOiCj70OpBJWFWKbCA'
-BOT_USERNAME = "Spidy_Series_bot"
+API_ID = '31445120' #bots
+API_HASH = '5e6c06ca38688ec6ec6e65dbff6d9b07'
+BOT_TOKEN = '8554616651:AAHwOIWuR4o17xA0vO7yLBJ_J67a2RCT084'
+BOT_USERNAME = "Spidy_Series_2_Bot"
 
 # Required Database and Channel Settings
-DATABASE_URI="mongodb://PreferableResponss:Germination@spidy_bots_speedy:27017/?tls=false"
+DATABASE_URI="mongodb+srv://spidy22:fklF8vAjZUp5Isq1@spidy22.vt63obr.mongodb.net/?retryWrites=true&w=majority&appName=Spidy22"
 DATABASE_URL="mongodb+srv://sp:sp@cluster0.eh3zdcl.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = "series_collection"
 COLLECTION_NAME = "series"
@@ -36,7 +36,7 @@ REQ_CHANNEL_TWO = (int(REQ_CHANNEL_TWO) if REQ_CHANNEL_TWO and id_pattern.search
 REQ_CHANNEL_THREE = environ.get("REQ_CHANNEL_THREE", None)
 REQ_CHANNEL_THREE = (int(REQ_CHANNEL_THREE) if REQ_CHANNEL_THREE and id_pattern.search(REQ_CHANNEL_THREE) else False) if REQ_CHANNEL_THREE is not None else None
 
-LOG_CHANNEL = "-1002361556192"
+LOG_CHANNEL = "-1003790556558"
 DB_CHANNEL = [-1002400599577, -1002964945426]
 RAW_DB_CHANNEL = [2400599577, 2964945426]
 IMGBB_API_KEY = "e74d34d56644c5a9543019e408dbd891"
